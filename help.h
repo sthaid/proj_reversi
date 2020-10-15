f5c2d7850ae08dd2b991b8d86bac7f2ad37d0633
Steven Haid <stevenhaid@gmail.com>
2020-10-14 20:34:12 -0400

    add help.h file

---
+++ help.h
@@ -0,0 +1,36 @@
+char help_text[] = 
+"\
+line 1\n\
+line 2\n\
+line 3\n\
+line 4\n\
+line 5\n\
+line 6\n\
+line 7\n\
+line 8\n\
+line 9\n\
+line 10\n\
+\n\
+line 1\n\
+line 2\n\
+line 3\n\
+line 4\n\
+line 5\n\
+line 6\n\
+line 7\n\
+line 8\n\
+line 9\n\
+line 10\n\
+\n\
+line 1\n\
+line 2\n\
+line 3\n\
+line 4\n\
+line 5\n\
+line 6\n\
+line 7\n\
+line 8\n\
+line 9\n\
+line 10\n\
+\n\
+";
